a19056a32b6b0472b613607b86073e00c1ce5ee9
Tyler <scottt2@uw.edu>
2019-07-08 15:40:46 -0400

    v1 done. Time to practice...

---
+++ hex_flash.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+_Bool correct_answer(int answer, int value) {
+  return answer == value;
+}
+
+void new_value(int *value) {
+  *value = rand() % 255;
+}
+
+int prompt_user(int value) {
+  printf("What is %02x: ", value);
+
+  int answer;
+  if (scanf("%d", &answer) != 1) {
+    while (fgetc(stdin) != '\n');
+    return -1;
+  }
+
+  return (answer >= 0 && answer <= 255)
+    ? answer
+    : -1;
+}
+
+void update_stats(double const time, int *rights, double *right_avg) {
+  *right_avg += time;
+  *right_avg /= (double) ++*rights;
+}
+
+int main(void) {
+  srand(time(0));
+
+  int value;
+  new_value(&value);
+
+  int rights = 0;
+  double right_avg = 0;
+  int wrongs = 0;
+
+  for (;;) {
+    time_t start = time(0);
+
+    if (correct_answer(prompt_user(value), value)) {
+      double time_spent = ((double) (time(0) - start));
+      update_stats(time_spent, &rights, &right_avg);
+
+      printf(
+        "\n\n*** Correct! ***\nYour score is %d\nYou average time is %g seconds\n\n",
+        rights,
+        right_avg
+      );
+
+      new_value(&value);
+    } else {
+      printf("Incorrect :(\n");
+
+      if (++wrongs > 2) {
+        printf("\n\nThe answer was %d.\n\n", value);
+        new_value(&value);
+        wrongs = 0;
+      }
+    }
+  }
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+P=hex_flash
+OBJECTS=
+CFLAGS= -g -Wall -std=c99
+LDLIBS=
+CC=clang
+
+$(P): $(OBJECTS)
